5aedc19423f5d14138072ee62b7b8de65d410ec0
河合結月 <oyuzu@kawaiketsugatsunoMacBook-Air.local>
2026-08-24 21:19:17 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# test
+print("hello world")
+
+
+def counting(endnum):
+    for num in range(endnum):
+        print(num)
+
+counting(10)
+print("done!!")
+counting(20)
+print("done!!")
